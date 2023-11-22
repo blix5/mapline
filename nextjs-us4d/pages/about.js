ae3c8e238d14666e2aac2448c741067cd36e28a3
blueberryperson5 <peter.lour@icloud.com>
2023-11-22 15:10:43 -0800

    fixed something oopsies 	modified:   nextjs-us4d/pages/about.js

--- nextjs-us4d/pages/about.js
+++ nextjs-us4d/pages/about.js
@@ -16,17 +16,6 @@ export default function About() {
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>pages</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
       </section>
     </Layout>
   );
